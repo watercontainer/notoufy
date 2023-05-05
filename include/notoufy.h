76e878de8f866ae68e37200267611989d64a63b3
Amrou <amrou.a84@gmail.com>
2023-05-05 22:57:04 +0200

    Reformat

--- include/notoufy.h
+++ include/notoufy.h
@@ -1,58 +1,57 @@
 #ifndef NOTOUF_NOTOUF_H
 #define NOTOUF_NOTOUF_H
 
-#include <vector>
-#include <memory>
 #include <concepts>
+#include <memory>
 #include <mutex>
+#include <vector>
 
 namespace notoufy {
 
-    class NullMutex {
-    public:
-        void lock() {};
-        void unlock() {};
-    };
-
-    using Multithreaded = std::mutex;
-
-    template<typename Ifc, typename Mutex = NullMutex>
-    class Notifier {
-    private:
-        std::vector<std::weak_ptr<Ifc>> observers;
-        Mutex m;
-    public:
-        void Register(std::weak_ptr<Ifc> obs) {
-            std::lock_guard lock(m);
-            observers.push_back(obs);
-        }
-
-        void Unregister(std::weak_ptr<Ifc> obs) {
-            const auto obsPtr = obs.lock();
-            std::lock_guard lock(m);
-            // Since c++20, std::erase_if replaces remove/erase idiom
-            std::erase_if(observers, [obsPtr](const auto &ptr) {
-                return (ptr.expired() || (obsPtr == ptr.lock()));
-            });
-        }
-
-        template<typename... F, typename... Arg>
-        // First arg is a Function M of signature void (Ifc::*)(F)
-        // that is a pointer to a function member of class Ifc
-        void Notify(void (Ifc::*M)(F...), Arg &&...args) {
-            std::lock_guard lock(m);
-            // take the opportunity to clean up any expired observers
-            std::erase_if(observers, [&M, &args...](const auto &obs) {
-                auto ptr = obs.lock();
-                if (ptr) {
-                    (ptr.get()->*M)(std::forward<Arg>(args)...);
-                    return false;
-                }
-                return true;
-            });
-        }
-    };
+class NullMutex {
+public:
+  void lock(){};
+  void unlock(){};
 };
 
+using Multithreaded = std::mutex;
 
-#endif //NOTOUF_NOTOUF_H
+template <typename Ifc, typename Mutex = NullMutex> class Notifier {
+private:
+  std::vector<std::weak_ptr<Ifc>> observers;
+  Mutex                           m;
+
+public:
+  void Register(std::weak_ptr<Ifc> obs) {
+    std::lock_guard lock(m);
+    observers.push_back(obs);
+  }
+
+  void Unregister(std::weak_ptr<Ifc> obs) {
+    const auto      obsPtr = obs.lock();
+    std::lock_guard lock(m);
+    // Since c++20, std::erase_if replaces remove/erase
+    std::erase_if(observers, [obsPtr](const auto &ptr) {
+      return (ptr.expired() || (obsPtr == ptr.lock()));
+    });
+  }
+
+  template <typename... F, typename... Arg>
+  // First arg is a Function M of signature void (Ifc::*)(F)
+  // that is a pointer to a function member of class Ifc
+  void Notify(void (Ifc::*M)(F...), Arg &&...args) {
+    std::lock_guard lock(m);
+    // take the opportunity to clean up any expired observers
+    std::erase_if(observers, [&M, &args...](const auto &obs) {
+      auto ptr = obs.lock();
+      if (ptr) {
+        (ptr.get()->*M)(std::forward<Arg>(args)...);
+        return false;
+      }
+      return true;
+    });
+  }
+};
+};     // namespace notoufy
+
+#endif // NOTOUF_NOTOUF_H
--- tests/test_notoufy.cpp
+++ tests/test_notoufy.cpp
@@ -9,138 +9,128 @@ using namespace notoufy;
 
 class INotifier {
 public:
-    virtual ~INotifier() = default;
+  virtual ~INotifier() = default;
 
-    virtual void onEvent() = 0;
-    virtual void onEventIncrement() = 0;
-    virtual void onEvent1(int arg) = 0;
+  virtual void onEvent()          = 0;
+  virtual void onEventIncrement() = 0;
+  virtual void onEvent1(int arg)  = 0;
 };
 
 class Observer : public INotifier {
 public:
-    virtual void onEvent() override {
-        notified = true;
-    }
+  virtual void onEvent() override { notified = true; }
 
-    virtual void onEventIncrement() override {
-        ++counter;
-    }
+  virtual void onEventIncrement() override { ++counter; }
 
-    virtual void onEvent1([[maybe_unused]] int arg) override {
-        notified = true;
-    }
+  virtual void onEvent1([[maybe_unused]] int arg) override { notified = true; }
 
-    bool called() const {
-        return notified;
-    }
+  bool called() const { return notified; }
 
-    const uint32_t& count() const {
-        return counter;
-    }
+  const uint32_t &count() const { return counter; }
 
 private:
-    bool notified = false;
-    uint32_t counter = 0;
+  bool     notified = false;
+  uint32_t counter  = 0;
 };
 
 TEST(Notifications, test_register) {
-    Notifier<INotifier> notification_bus;
+  Notifier<INotifier> notification_bus;
 
-    auto observer = std::make_shared<Observer>();
-    notification_bus.Notify(&INotifier::onEvent);
-    ASSERT_FALSE(observer->called());
+  auto observer = std::make_shared<Observer>();
+  notification_bus.Notify(&INotifier::onEvent);
+  ASSERT_FALSE(observer->called());
 
-    notification_bus.Register(observer);
-    notification_bus.Notify(&INotifier::onEvent);
-    ASSERT_TRUE(observer->called());
+  notification_bus.Register(observer);
+  notification_bus.Notify(&INotifier::onEvent);
+  ASSERT_TRUE(observer->called());
 }
 
 TEST(Notifications, test_unregister) {
-    Notifier<INotifier> notification_bus;
-    auto observer = std::make_shared<Observer>();
+  Notifier<INotifier> notification_bus;
+  auto                observer = std::make_shared<Observer>();
 
-    notification_bus.Register(observer);
-    notification_bus.Unregister(observer);
-    notification_bus.Notify(&INotifier::onEvent);
-    ASSERT_FALSE(observer->called());
+  notification_bus.Register(observer);
+  notification_bus.Unregister(observer);
+  notification_bus.Notify(&INotifier::onEvent);
+  ASSERT_FALSE(observer->called());
 }
 
 TEST(Notifications, test_notify_one_no_args) {
-    Notifier<INotifier> notification_bus;
+  Notifier<INotifier> notification_bus;
 
-    auto observer = std::make_shared<Observer>();
-    notification_bus.Register(observer);
-    notification_bus.Notify(&INotifier::onEvent);
+  auto observer = std::make_shared<Observer>();
+  notification_bus.Register(observer);
+  notification_bus.Notify(&INotifier::onEvent);
 
-    ASSERT_TRUE(observer->called());
+  ASSERT_TRUE(observer->called());
 }
 
 TEST(Notifications, test_notify_multiple_no_args) {
-    Notifier<INotifier> notification_bus;
+  Notifier<INotifier> notification_bus;
 
-    std::vector<std::shared_ptr<Observer>> observers;
-    for (auto i = 0; i < 10; ++i) {
-        observers.emplace_back(std::make_shared<Observer>());
-        notification_bus.Register(observers[i]);
-    }
+  std::vector<std::shared_ptr<Observer>> observers;
+  for (auto i = 0; i < 10; ++i) {
+    observers.emplace_back(std::make_shared<Observer>());
+    notification_bus.Register(observers[i]);
+  }
 
-    notification_bus.Notify(&INotifier::onEvent);
+  notification_bus.Notify(&INotifier::onEvent);
 
-    for (auto &obs: observers) {
-        ASSERT_TRUE(obs->called());
-    }
+  for (auto &obs : observers) {
+    ASSERT_TRUE(obs->called());
+  }
 }
 
 TEST(Notifications, test_notify_one_arg) {
-    Notifier<INotifier> notification_bus;
+  Notifier<INotifier> notification_bus;
 
-    auto observer = std::make_shared<Observer>();
-    notification_bus.Register(observer);
-    notification_bus.Notify(&INotifier::onEvent1, 0xcafe);
+  auto observer = std::make_shared<Observer>();
+  notification_bus.Register(observer);
+  notification_bus.Notify(&INotifier::onEvent1, 0xcafe);
 
-    ASSERT_TRUE(observer->called());
+  ASSERT_TRUE(observer->called());
 }
 
 TEST(Notifications, test_notify_multiple_arg) {
-    Notifier<INotifier> notification_bus;
+  Notifier<INotifier> notification_bus;
 
-    std::vector<std::shared_ptr<Observer>> observers;
-    for (auto i = 0; i < 10; ++i) {
-        observers.emplace_back(std::make_shared<Observer>());
-        notification_bus.Register(observers[i]);
-    }
+  std::vector<std::shared_ptr<Observer>> observers;
+  for (auto i = 0; i < 10; ++i) {
+    observers.emplace_back(std::make_shared<Observer>());
+    notification_bus.Register(observers[i]);
+  }
 
-    notification_bus.Notify(&INotifier::onEvent1, 0xcafe);
+  notification_bus.Notify(&INotifier::onEvent1, 0xcafe);
 
-    for (auto &obs: observers) {
-        ASSERT_TRUE(obs->called());
-    }
+  for (auto &obs : observers) {
+    ASSERT_TRUE(obs->called());
+  }
 }
 
 TEST(Notifications, test_notify_one_count_multithreaded) {
-    Notifier<INotifier, Multithreaded> notification_bus;
+  Notifier<INotifier, Multithreaded> notification_bus;
 
-    auto observer = std::make_shared<Observer>();
-    notification_bus.Register(observer);
+  auto observer = std::make_shared<Observer>();
+  notification_bus.Register(observer);
 
-    // Spawn multiple thread to increment the same variable.
-    std::vector<std::thread> threads;
-    for (int i = 0 ; i < 10 ; ++i) {
-        threads.emplace_back([&notification_bus]() {
-            for (int j = 0; j < 10000 ; ++j) {
-                notification_bus.Notify(&INotifier::onEventIncrement);
-            }
-        });
-    }
+  // Spawn multiple thread to increment the same variable.
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 10; ++i) {
+    threads.emplace_back([&notification_bus]() {
+      for (int j = 0; j < 10000; ++j) {
+        notification_bus.Notify(&INotifier::onEventIncrement);
+      }
+    });
+  }
 
-    for (auto& thread:threads) {
-        thread.join();
-    }
+  for (auto &thread : threads) {
+    thread.join();
+  }
 
-    ASSERT_EQ(observer->count(), 100000);
+  ASSERT_EQ(observer->count(), 100000);
 }
 
 int main() {
-    ::testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
